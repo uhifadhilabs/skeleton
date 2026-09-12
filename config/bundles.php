d2f8168eab6b85ee58046c3d8c88c822ccd81c11
Ezekiel Emil Mjema <mjema.ezekiel@gmail.com>
2026-09-12 14:29:01 +0200

    The starter installs the core that answers the handset, and states the field API's one format

--- config/bundles.php
+++ config/bundles.php
@@ -1,16 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
-/*
- * This file is part of uhifadhi.
- *
- * (c) Ezekiel Mjema <https://github.com/eemjema>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
@@ -27,4 +16,5 @@ return [
     Uhifadhi\Bundle\AtlasBundle\AtlasBundle::class => ['all' => true],
     Uhifadhi\Bundle\TeamBundle\TeamBundle::class => ['all' => true],
     Uhifadhi\Bundle\AreaBundle\AreaBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
